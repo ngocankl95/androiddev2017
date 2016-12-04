fa13d8473fca485191bda89c0bfc8c3fca36f9dc
huy1395 <doomlord1395@gmail.com>
2016-12-04 12:40:35 +0700

    add activity for galerry

--- Flickr/app/src/main/java/flickr/usth/edu/com/PhotoGalleryActivity.java
+++ Flickr/app/src/main/java/flickr/usth/edu/com/PhotoGalleryActivity.java
@@ -2,8 +2,8 @@ package flickr.usth.edu.com;
 
 import android.support.v4.app.Fragment;
 
-import huy.photogallery.Fragment.PhotoGalleryFragment;
-import huy.photogallery.Fragment.SingleFragmentActivity;
+import  flickr.usth.edu.com.Fragment.PhotoGalleryFragment;
+import  flickr.usth.edu.com.Fragment.SingleFragmentActivity;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
