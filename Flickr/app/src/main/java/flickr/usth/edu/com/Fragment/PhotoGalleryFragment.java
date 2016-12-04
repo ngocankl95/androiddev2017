18ceba557864b919bcd3e7138d1de8f92d61015c
An Ngoc <ngocan.4595@gmail.com>
2016-12-04 08:20:35 +0700

    add gallery fragment

---
+++ Flickr/app/src/main/java/flickr/usth/edu/com/Fragment/PhotoGalleryFragment.java
@@ -0,0 +1,154 @@
+package flickr.usth.edu.com.Fragment;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import huy.photogallery.GalleryItem;
+import huy.photogallery.Network.FlickrFetchr;
+import huy.photogallery.R;
+import huy.photogallery.ThumbnailDownloader;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by ngoca on 12/1/2016.
+ */
+
+public class PhotoGalleryFragment extends android.support.v4.app.Fragment {
+
+    private RecyclerView mPhotoRecyclerView;
+    private  List<GalleryItem> mItems = new ArrayList<>();
+    private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
+
+    public static PhotoGalleryFragment newInstance(){
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemTask().execute();
+
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap bitmap) {
+                        Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                        photoHolder.bindDrawable(drawable);
+                    }
+                }
+        );
+        mThumbnailDownloader.start();
+        mThumbnailDownloader.getLooper();
+        Log.i(TAG, "bg thread started");
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder{
+        private ImageView mItemImageView;
+
+        public PhotoHolder(View itemView){
+            super(itemView);
+            mItemImageView = (ImageView) itemView
+                    .findViewById(R.id.fragment_photo_gallery_image_view);
+        }
+
+        public  void bindDrawable(Drawable drawable){
+            mItemImageView.setImageDrawable(drawable);
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
+        private List<GalleryItem> mGalleryItems;
+
+        public  PhotoAdapter(List<GalleryItem> galleryItems){
+            mGalleryItems = galleryItems;
+        }
+
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(R.layout.gallery_item, viewGroup, false);
+            return new PhotoHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
+            GalleryItem galleryItem = mGalleryItems.get(position);
+            Drawable placeholder = getResources().getDrawable(R.drawable.and);
+            photoHolder.bindDrawable(placeholder);
+            mThumbnailDownloader.queueThumbnail(photoHolder, galleryItem.getmUrl());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = (RecyclerView) v
+                .findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        setupAdapter();
+        return v;
+    }
+
+    private void setupAdapter() {
+        if (isAdded()){
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.cleanQueue();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mThumbnailDownloader.quit();
+        Log.i(TAG, "bg thread ended");
+    }
+
+    private class FetchItemTask extends AsyncTask<Void,Void, List<GalleryItem>>{
+        @Override
+        protected List<GalleryItem> doInBackground(Void... params) {            /**
+         try {
+         String result =new FlickrFetchr()
+         .getUrlString("https://www.bignerdranch.com");
+         Log.i(TAG, "Fetched contents of URL: " + result);
+
+         } catch (IOException ioe){
+         Log.e(TAG, "Failed to fetch URL: ", ioe);
+         }*/
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items){
+            mItems = items;
+            setupAdapter();
+        }
+    }
+}
--- Flickr/app/src/main/java/flickr/usth/edu/com/MainActivity.java
+++ Flickr/app/src/main/java/flickr/usth/edu/com/MainActivity.java
@@ -1,73 +1,14 @@
 package flickr.usth.edu.com;
 
-import android.content.Intent;
-import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.GridView;
+import android.support.v4.app.Fragment;
 
-import java.util.ArrayList;
+import huy.photogallery.Fragment.PhotoGalleryFragment;
+import huy.photogallery.Fragment.SingleFragmentActivity;
 
-import flickr.usth.edu.com.file.upload.CameraActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    private GridView gridView;
-    private GridViewAdapter gridAdapter;
-    private Button btnGetMedia;
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        gridView = (GridView) findViewById(R.id.gridView);
-//        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
-        gridView.setAdapter(gridAdapter);
-        btnGetMedia = (Button) findViewById(R.id.btnGetMedia);
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
-                //Create intent
-                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
-                intent.putExtra("title", item.getTitle());
-                intent.putExtra("image", item.getImage());
-
-                //Start details activity
-                startActivity(intent);
-            }
-        });
-
-        /**
-         * Get media button click event
-         */
-        btnGetMedia.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                //Create intent
-                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-
-                //Start details activity
-                startActivity(intent);
-            }
-        });
-    }
-
-    // Prepare some dummy data for gridview
-    private ArrayList<ImageItem> getData() {
-        final ArrayList<ImageItem> imageItems = new ArrayList<>();
-        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-        for (int i = 0; i < imgs.length(); i++) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-            imageItems.add(new ImageItem(bitmap, "Image#" + i));
-        }
-        return imageItems;
+    public Fragment createFragment(){
+        return PhotoGalleryFragment.newInstance();
     }
 }
