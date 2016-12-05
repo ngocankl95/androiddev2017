67e5dc80a976354354e2cf5f12795d07c90c3fee
huy1395 <DoomLord1395@gmail.com>
2016-12-06 01:06:40 +0700

    adding link to site

--- Flickr/app/src/main/java/flickr/usth/edu/com/Fragment/PhotoGalleryFragment.java
+++ Flickr/app/src/main/java/flickr/usth/edu/com/Fragment/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package flickr.usth.edu.com.Fragment;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -35,11 +36,6 @@ import static android.content.ContentValues.TAG;
 
 public class PhotoGalleryFragment extends android.support.v4.app.Fragment {
 
-<<<<<<< HEAD
-    private static final String TAG = "PhotoGalleryFragment";
-
-=======
->>>>>>> bea0b26a8b0a1e180788dafa887dd8ccf5cc8084
     private RecyclerView mPhotoRecyclerView;
     private  List<GalleryItem> mItems = new ArrayList<>();
     private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
@@ -72,17 +68,29 @@ public class PhotoGalleryFragment extends android.support.v4.app.Fragment {
         Log.i(TAG, "bg thread started");
     }
 
-    private class PhotoHolder extends RecyclerView.ViewHolder{
+    private class PhotoHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private GalleryItem mGalleryItem;
         private ImageView mItemImageView;
 
         public PhotoHolder(View itemView){
             super(itemView);
             mItemImageView = (ImageView) itemView
                     .findViewById(R.id.fragment_photo_gallery_image_view);
+            itemView.setOnClickListener(this);
         }
 
-        public void bindDrawable(Drawable drawable){
+        public  void bindDrawable(Drawable drawable){
             mItemImageView.setImageDrawable(drawable);
+        }
+
+        public void bindGalleryItem(GalleryItem galleryItem){
+            mGalleryItem = galleryItem;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(Intent.ACTION_VIEW, mGalleryItem.getPhotoPageUri());
+            startActivity(i);
         }
     }
 
@@ -103,6 +111,7 @@ public class PhotoGalleryFragment extends android.support.v4.app.Fragment {
         @Override
         public void onBindViewHolder(PhotoHolder photoHolder, int position) {
             GalleryItem galleryItem = mGalleryItems.get(position);
+            photoHolder.bindGalleryItem(galleryItem);
             Drawable placeholder = getResources().getDrawable(R.drawable.timer);
             photoHolder.bindDrawable(placeholder);
             mThumbnailDownloader.queueThumbnail(photoHolder, galleryItem.getmUrl());
@@ -204,7 +213,7 @@ public class PhotoGalleryFragment extends android.support.v4.app.Fragment {
         protected List<GalleryItem> doInBackground(Void... params) {
 
             if (mQuery == null){
-                return  new  FlickrFetchr().fetchRecentPhotos();
+                return  new FlickrFetchr().fetchRecentPhotos();
             } else {
                 return new FlickrFetchr().searchPhotos(mQuery);
             }
--- Flickr/app/src/main/java/flickr/usth/edu/com/Network/FlickrFetchr.java
+++ Flickr/app/src/main/java/flickr/usth/edu/com/Network/FlickrFetchr.java
@@ -123,6 +123,7 @@ public class FlickrFetchr {
                 continue;
             }
             item.setUrl(photoJsonObject.getString("url_s"));
+            item.setOwner(photoJsonObject.getString("owner"));
             items.add(item);
         }
     }
