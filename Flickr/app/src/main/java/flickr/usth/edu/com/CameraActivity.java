b4357026af4fbf7c14d1094e0411c773639c1176
An Ngoc <ngocan.4595@gmail.com>
2016-12-06 08:32:00 +0700

    add config for dir, set output dir after take photo

--- Flickr/app/src/main/java/flickr/usth/edu/com/CameraActivity.java
+++ Flickr/app/src/main/java/flickr/usth/edu/com/CameraActivity.java
@@ -191,4 +191,55 @@ public class CameraActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void launchUploadActivity(boolean isImage){
+        Intent i = new Intent(CameraActivity.this, UploadActivity.class);
+        i.putExtra("filePath", fileUri.getPath());
+        i.putExtra("isImage", isImage);
+        startActivity(i);
+    }
+
+    /**
+     * Creating file uri to store image/video
+     */
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /**
+     * returning image / video
+     */
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                Config.IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "Oops! Failed create "
+                        + Config.IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
 }
---
+++ Flickr/app/src/main/java/flickr/usth/edu/com/Config.java
@@ -0,0 +1,10 @@
+package flickr.usth.edu.com;
+
+/**
+ * Created by ngoca on 6/12/2016.
+ */
+
+public class Config {
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
